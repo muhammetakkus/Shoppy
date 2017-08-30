c7bf707d2e25c7825a921543539d1c8822406cca
muhammetakkus <muhammetakkuss34@gmail.com>
2017-08-30 03:25:03 +0300

    Final Commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,27 @@
-import {qs, on} from './app/helpers'
-import Events from './app/event'
 import render from './app/render'
+import Events from './app/event'
 import Cart from './app/cart'
 import Menu from './app/menu'
 
 /* Render Storaged Data */
 render();
 
-/* Events - bu fonkiyonlar bir classın constructerında çalıştırılsa burası daha temiz olsa?*/
+/* Events - bu fonkiyonlar bir classın constructerında çalıştırılsa burası daha temiz olsa? */
 let Event = new Events();
+
 /* Add Item */
 Event.onKeyPress();
 Event.addButtonClick();
+
 /* Check Item */
 Event.checkEvent();
+
 /* Delete Item */
 Event.deleteEvent();
+
 /* Delete Cart */
 Event.deleteCart();
+
 /* Select Another Cart */
 Event.selectCart();
 
@@ -27,20 +31,9 @@ new Cart();
 /* Menu Toggle */
 new Menu();
 
-//bu storage daki getAll nereden nasıl çalışıyor sayfa açılışında?
-//deleteCart() -> eğer seçili kart siliniyorsa yeni seçilen carttaki to-do-itemler listelenirken toDoList() kullanılıyor burada sidebar hide oluyor
-//aynı şekilde selectCart yapıldığında cart itemleri boş ise toDoList() fonks. çalıştırılmıyor item varsa çalışınca sidebar hide oluyor
+/*
+  deleteCart() -> eğer seçili kart siliniyorsa yeni seçilen carttaki to-do-itemler listelenirken toDoList() kullanılıyor burada sidebar hide oluyor
+  aynı şekilde selectCart yapıldığında cart itemleri boş ise toDoList() fonks. çalıştırılmıyor item varsa çalışınca sidebar hide oluyor
+*/
 
-
-/**
- * JS WORK
- * dökümantasyon
- * */
-
- /* Kaynak kodu okuma trickleri */
- /**
-  * çok kullanılan düğüm noktaları bul oradan başla
-  * bir library inceleyeceksen eski sürümlerinden birine git ile ulaş onu oku ~ basic halinin okumak için
-  * dosya isimlerinden dosyaların bir biri ile ilişkisi düşünülebilir
-  * en çok bağımlılık olan dosyalara bakılabilir
-  */
+//Firebase Integration
